f8f0a513663a3eba9c4457fbbfe158fa405db7d8
rahulbhargava010 <rahulbhargava010@gmail.com>
2023-08-03 23:25:48 +0530

    added more featurs to table and conversation view fixed

--- src/pages/Conversation.js
+++ src/pages/Conversation.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import SideBar from "../components/SideBar";
 import axios from "axios";
 import { SearchOutlined } from '@ant-design/icons';
@@ -6,17 +7,68 @@ import {
     MenuFoldOutlined,
     MenuUnfoldOutlined, MailOutlined, BellOutlined
 } from '@ant-design/icons';
-import { Layout, Button, theme, Space, Badge, Card, Divider, Statistic, Table, Typography, Col, Row, Input, InputRef } from "antd";
+import { Layout, Button, theme, Space, Badge, Card, Tag, Table, Input, Modal, DatePicker } from "antd";
 import { useState, useEffect, useRef } from "react";
+// import moment from 'moment';
 
-import { useNavigate, NavLink } from 'react-router-dom';
+// import { useNavigate, NavLink } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Form from 'react-bootstrap/Form';
 import Highlighter from 'react-highlight-words';
+import { v4 as uuid } from 'uuid';
+
+import './conversation.css';
 
 const { Header, Content } = Layout;
 
+// const initialDate = moment();
+
+// Define the desired date format
+// const dateFormat = 'YYYY-MM-DD';
+
+var stylingObject = {
+    rowC: {
+        display: "flex",
+        flexDirection: "row",
+        marginTop: '15px'
+    },
+    chatStart: {
+        padding: "25px",
+        display: "inline-block",
+        /* min-height: 380px; */
+        /* max-height: 380px; */
+        background: "#fbfbfb",
+        overflow: "auto"
+    },
+
+    chatBubbleYou: {
+        backgroundImage: "linear-gradient(to right, #dee3e8, #d8dadd, #d2ccd6, #babfc4, #acb0b3)!important",
+        color: "black !important",
+        borderRadius: "0 15px 15px 15px",
+        alignSelf: "flex-start",
+        display: "table"
+    },
+    
+    chatBubbleMe: {
+        backgroundImage: "linear-gradient(to right, #FFFFFF, #FFFFFF, #FFFFFF, #FFFFFF, #FFFFFF)",
+        color: "#888ea8",
+        borderRadius: "15px 0px 15px 15px",
+        float: "right",
+        alignSelf: "flex-end",
+        display: "table"
+    },
+    
+    chatBubble: {
+      fontSize: "12px",
+      padding: "10px 15px",
+      display: "inline-block",
+      clear: "both",
+      marginBottom: "10px",
+      boxShadow: "0 5px 5px rgb(0 0 0 / 2%)"
+    }
+
+}
 
 function Conversation(){
     const[collapsed, setCollapsed] = useState(false)
@@ -27,6 +79,8 @@ function Conversation(){
     const [conversations, setConversations] = useState([])
     const [botCount, setbotCount] = useState(0)
     const [conversationsCount, setConversationsCount] = useState(0)
+    const [open, setOpen] = useState(false);
+    const [conversationHTML, setConversationHTML] = useState('')
 
     useEffect(() => {
       setUser(JSON.parse(localStorage.getItem("user_token")))
@@ -81,6 +135,7 @@ function Conversation(){
       })
       .then(response => {
           if(response.status === 200) {
+              // console.log('response.data?.result', response.data?.result)
               setConversations(response.data?.result)
               setConversationsCount(response.data?.result?.length)
               toast.success(response.data?.msg, {
@@ -211,53 +266,85 @@ function Conversation(){
         ),
     });
 
-    const columns = [{
-      title : 'Name',
-      dataIndex : 'name',
-      key : 'name',
-      ...getColumnSearchProps('name'),
+    const handleConversation = (conversation) => {
+      setOpen(true)
+      setConversationHTML(conversation)
+      // const newData = dataSource.filter((item) => item.key !== key);
+      // setDataSource(newData);
+    };
 
-    },
-    {
-      title : 'Email',
-      dataIndex : 'email',
-      key : 'email',
-      ...getColumnSearchProps('email'),
-    },
-    {
-      title : 'Conversation',
-      dataIndex : 'conversation',
-      key : 'conversation'
-    },
-    {
-      title : 'Source',
-      dataIndex : 'source',
-      key : 'source',
-      ...getColumnSearchProps('source'),
-    },
-    {
-      title : 'Sub Source',
-      dataIndex : 'subSource',
-      key : 'subSource',
-      ...getColumnSearchProps('subSource'),
-    },
-    {
-      title : 'URL',
-      dataIndex : 'url',
-      key : 'url'
-    },
-    {
-      title : 'Ref URL',
-      dataIndex : 'refURL',
-      key : 'refURL'
-    },
-    {
-      title : 'Date',
-      dataIndex : 'added',
-      key : 'added',
-      sorter: (a, b) => a.added.length - b.added.length,
-      sortDirections: ['descend', 'ascend'],
-  }]
+    const columns = [
+      { 
+        key: React.Key,
+      },
+
+      {
+        title : 'Name',
+        dataIndex : 'name',
+        key : 'name',
+        ...getColumnSearchProps('name'),
+
+      },
+      {
+        title : 'Email',
+        dataIndex : 'email',
+        key : 'email',
+        ...getColumnSearchProps('email'),
+      },
+      // {
+      //   title : 'Conversation',
+      //   dataIndex : 'conversation',
+      //   key : 'conversation'
+      // },
+      {
+        title : 'Source',
+        dataIndex : 'source',
+        key : 'source',
+        ...getColumnSearchProps('source'),
+      },
+      {
+        title : 'Sub Source',
+        dataIndex : 'subSource',
+        key : 'subSource',
+        ...getColumnSearchProps('subSource'),
+      },
+      // {
+      //   title : 'URL',
+      //   dataIndex : 'url',
+      //   key : 'url'
+      // },
+      // {
+      //   title : 'Ref URL',
+      //   dataIndex : 'refURL',
+      //   key : 'refURL'
+      // },
+      {
+        title : 'Date',
+        dataIndex : 'added',
+        key : 'added',
+        sorter: (a, b) => a.added.length - b.added.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Replies',
+        dataIndex: '',
+        key: 'x',
+        render: (_, record) =>
+        conversations.length >= 1 ? (
+          <Tag color='#2db7f5' key={record._id} onClick={() => handleConversation(record)}>{(decodeURIComponent(record.conversation).match(new RegExp("chat-bubble me", "g")) || []).length}</Tag>
+        ) : null,
+      },
+      {
+        title: 'Action',
+        dataIndex: '',
+        key: 'x',
+        // render: () => <a>View Conversation</a>,
+        render: (_, record) =>
+        conversations.length >= 1 ? (
+            <Tag color='#f50' key={record._id} onClick={() => handleConversation(record)}>Conversation</Tag>
+          ) : null,
+      },
+    ]
 
     // ------------------ END Table Code   --------------------------------
 
@@ -271,9 +358,8 @@ function Conversation(){
               
               <Header
                   style={{
-                      padding: 0,
-                      background: colorBgContainer,
-              
+                    padding: 0,
+                    background: colorBgContainer,
                   }}
                   >
                   <Button 
@@ -300,7 +386,7 @@ function Conversation(){
                   </Space>
                   </div>
                   
-                </Header>
+              </Header>
                 
                 <Content
                     style={{
@@ -332,12 +418,43 @@ function Conversation(){
                         el.added_printabble = day + "/" + month + "/" + year;
                     })
                 } */}
+
+                          {/* <table>
+                            <tr>
+                              <td>URL</td>
+                              <td>{record.url}</td>
+                            </tr>
+                            <tr>
+                              <td>Ref URL</td>
+                              <td>{record.url}</td>
+                            </tr>
+                           
+                          </table> */}
+                          <Modal
+                            title={conversationHTML?.name}
+                            centered
+                            open={open}
+                            onOk={() => setOpen(false)}
+                            onCancel={() => setOpen(false)}
+                            width={1000}
+                          >
+                            {/* { decodeURIComponent(conversationHTML?.conversation) } */}
+                            <div className="chat-body-react" dangerouslySetInnerHTML = {{__html: decodeURIComponent(conversationHTML?.conversation)} } ></div>
+                          </Modal>
             
                   {
                     conversations.length ? (
                       <Table columns={columns}
                         dataSource={conversations}
                         style={{marginRight : 20}}
+                        bordered
+                        expandable={{
+                          expandedRowRender: (record) => <p style={{ margin: 0 }}>{<>
+                            <h6>URL: <a href={record.url} ><i>{record.url}</i></a></h6>
+                            <h6>Referer URL: <a href={record.refURL} ><i>{record.refURL}</i></a></h6>
+                          </>}</p>,
+                          rowExpandable: (record) => record._id !== 'Not Expandable',
+                        }}
                         >
                       </Table>
                     ) : ''
